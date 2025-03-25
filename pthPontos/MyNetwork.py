25e800e3c69a6605f7d9be6221dbcb7734704324
angelodev-new <angelo.dev.santos@gmail.com>
2025-03-25 17:18:41 -0300

    mynetwork class add

---
+++ pthPontos/MyNetwork.py
@@ -0,0 +1,9 @@
+from src.nets import deeplabv3plus_resnet50
+import torch
+import torch.nn as nn
+def getModel(path, num_classes=3, in_channels=3):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    net = deeplabv3plus_resnet50(num_classes=num_classes)
+    net = net.to(device)
+    net.backbone.conv1 = nn.Conv2d(in_channels, net.backbone.conv1.out_channels, kernel_size=7, stride=2, padding=3, bias=False, device=device)
+    return net
